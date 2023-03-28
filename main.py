14e4e31a6cabb26f3d8736ea88d67860dbfc18f7
monemax <mone9732@gmail.com>
2023-03-29 00:19:17 +0530

    first pesudo

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+# Selection Sort
+def sortlist(L,n):
+    L2 = []
+    counter = 0
+    while (counter <n):
+        m, idx = searchMinFromList (L,n)
+        L2.append(m)
+        del L[idx]
+        n = n-1
+        return L2
